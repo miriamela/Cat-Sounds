5e20c97a21c726f24060fd4441c21c0f8acc0bcc
miriamela <grossi.miriam@gmail.com>
2020-10-23 16:41:11 +0200

    edited console.log

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ function handleEvent(event) {
   audio.currentTime = 0; //rewind to start if pressing the same key before the audio finish
   audio.play(); //rules for .play(): first the audios need to be in the same folder as the app, in case they are not it needs the crossOrigin="anonymous" inside audio tag, but in my case even with the crossOrigin, it wasn't working... so better to have the audios inside the folder.
   const key = document.querySelector(`.key[data-key ="${event.keyCode}"]`);
-  console.log(key);
-  console.log(key.id);
+  // console.log(key);
+  // console.log(key.id);
   if (key.id % 2 === 0) {
     key.classList.add("color1");
   } else {
